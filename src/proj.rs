0ec7b2d436444a8bf2947bf1ab9c5af3da6d1983
drewhk <drewhk@drewhk.com>
2025-08-19 13:25:41 +0200

    Added support for 3D points

--- src/proj.rs
+++ src/proj.rs
@@ -1,6 +1,6 @@
 use libc::c_int;
 use libc::{c_char, c_double};
-use num_traits::Float;
+use num_traits::{Float, Zero};
 use proj_sys::{
     proj_area_create, proj_area_destroy, proj_area_set_bbox, proj_as_projjson, proj_as_wkt,
     proj_cleanup, proj_context_clone, proj_context_create, proj_context_destroy,
@@ -32,7 +32,7 @@ use std::mem::MaybeUninit;
 use std::path::Path;
 use thiserror::Error;
 
-pub trait CoordinateType: Float + Copy + PartialOrd + Debug {}
+pub trait CoordinateType: Float + Zero + Copy + PartialOrd + Debug {}
 impl<T: Float + Copy + PartialOrd + Debug> CoordinateType for T {}
 
 /// An error number returned from a PROJ call.
@@ -72,7 +72,8 @@ where
 {
     fn x(&self) -> T;
     fn y(&self) -> T;
-    fn from_xy(x: T, y: T) -> Self;
+    fn z(&self) -> T;
+    fn from_xyz(x: T, y: T, z: T) -> Self;
 }
 
 impl<T: CoordinateType> Coord<T> for (T, T) {
@@ -82,8 +83,22 @@ impl<T: CoordinateType> Coord<T> for (T, T) {
     fn y(&self) -> T {
         self.1
     }
-    fn from_xy(x: T, y: T) -> Self {
+    fn z(&self) -> T { T::zero() }
+    fn from_xyz(x: T, y: T, _z: T) -> Self {
         (x, y)
+    }
+}
+
+impl<T: CoordinateType> Coord<T> for (T, T, T) {
+    fn x(&self) -> T {
+        self.0
+    }
+    fn y(&self) -> T {
+        self.1
+    }
+    fn z(&self) -> T { self.2 }
+    fn from_xyz(x: T, y: T, z: T) -> Self {
+        (x, y, z)
     }
 }
 
@@ -917,8 +932,10 @@ impl Proj {
         };
         let c_x: c_double = point.x().to_f64().ok_or(ProjError::FloatConversion)?;
         let c_y: c_double = point.y().to_f64().ok_or(ProjError::FloatConversion)?;
+        let c_z: c_double = point.z().to_f64().ok_or(ProjError::FloatConversion)?;
         let new_x;
         let new_y;
+        let new_z;
         let err;
         // Input coords are defined in terms of lambda & phi, using the PJ_LP struct.
         // This signals that we wish to project geodetic coordinates.
@@ -928,7 +945,7 @@ impl Proj {
         let coords = PJ_LPZT {
             lam: c_x,
             phi: c_y,
-            z: 0.0,
+            z: c_z,
             t: f64::INFINITY,
         };
         unsafe {
@@ -936,14 +953,16 @@ impl Proj {
             // PJ_DIRECTION_* determines a forward or inverse projection
             let trans = proj_trans(self.c_proj, inv, PJ_COORD { lpzt: coords });
             // output of coordinates uses the PJ_XY struct
-            new_x = trans.xy.x;
-            new_y = trans.xy.y;
+            new_x = trans.xyz.x;
+            new_y = trans.xyz.y;
+            new_z = trans.xyz.z;
             err = proj_errno(self.c_proj);
         }
         if err == 0 {
-            Ok(Coord::from_xy(
+            Ok(Coord::from_xyz(
                 F::from(new_x).ok_or(ProjError::FloatConversion)?,
                 F::from(new_y).ok_or(ProjError::FloatConversion)?,
+                F::from(new_z).ok_or(ProjError::FloatConversion)?,
             ))
         } else {
             Err(ProjError::Projection(error_message(err)?))
@@ -991,8 +1010,10 @@ impl Proj {
     {
         let c_x: c_double = point.x().to_f64().ok_or(ProjError::FloatConversion)?;
         let c_y: c_double = point.y().to_f64().ok_or(ProjError::FloatConversion)?;
+        let c_z: c_double = point.z().to_f64().ok_or(ProjError::FloatConversion)?;
         let new_x;
         let new_y;
+        let new_z;
         let err;
 
         // This doesn't seem strictly correct, but if we set PJ_XY or PJ_LP here, the
@@ -1001,20 +1022,22 @@ impl Proj {
         let xyzt = PJ_XYZT {
             x: c_x,
             y: c_y,
-            z: 0.0,
+            z: c_z,
             t: f64::INFINITY,
         };
         unsafe {
             proj_errno_reset(self.c_proj);
             let trans = proj_trans(self.c_proj, PJ_DIRECTION_PJ_FWD, PJ_COORD { xyzt });
-            new_x = trans.xy.x;
-            new_y = trans.xy.y;
+            new_x = trans.xyz.x;
+            new_y = trans.xyz.y;
+            new_z = trans.xyz.z;
             err = proj_errno(self.c_proj);
         }
         if err == 0 {
-            Ok(C::from_xy(
+            Ok(C::from_xyz(
                 F::from(new_x).ok_or(ProjError::FloatConversion)?,
                 F::from(new_y).ok_or(ProjError::FloatConversion)?,
+                F::from(new_z).ok_or(ProjError::FloatConversion)?,
             ))
         } else {
             Err(ProjError::Conversion(error_message(err)?))
@@ -1206,11 +1229,12 @@ impl Proj {
             .map(|point| {
                 let c_x: c_double = point.x().to_f64().ok_or(ProjError::FloatConversion)?;
                 let c_y: c_double = point.y().to_f64().ok_or(ProjError::FloatConversion)?;
+                let c_z: c_double = point.z().to_f64().ok_or(ProjError::FloatConversion)?;
                 Ok(PJ_COORD {
                     xyzt: PJ_XYZT {
                         x: c_x,
                         y: c_y,
-                        z: 0.0,
+                        z: c_z,
                         t: f64::INFINITY,
                     },
                 })
@@ -1237,9 +1261,10 @@ impl Proj {
             // feels a bit clunky, but we're guaranteed that pj and points have the same length
             unsafe {
                 for (i, coord) in pj.iter().enumerate() {
-                    points[i] = Coord::from_xy(
-                        F::from(coord.xy.x).ok_or(ProjError::FloatConversion)?,
-                        F::from(coord.xy.y).ok_or(ProjError::FloatConversion)?,
+                    points[i] = Coord::from_xyz(
+                        F::from(coord.xyz.x).ok_or(ProjError::FloatConversion)?,
+                        F::from(coord.xyz.y).ok_or(ProjError::FloatConversion)?,
+                        F::from(coord.xyz.z).ok_or(ProjError::FloatConversion)?,
                     )
                 }
             }
@@ -1540,8 +1565,11 @@ mod test {
         fn y(&self) -> f64 {
             self.y
         }
+        fn z(&self) -> f64 {
+            0.0
+        }
 
-        fn from_xy(x: f64, y: f64) -> Self {
+        fn from_xyz(x: f64, y: f64, _z: f64) -> Self {
             MyPoint { x, y }
         }
     }
